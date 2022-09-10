7f629a87749229e95c9555e802480590156978e6
neglux <akcaaytuna@gmail.com>
2022-09-10 20:44:09 +0300

    adding problem solution

---
+++ P3-largestprimefactor/script.js
@@ -0,0 +1,25 @@
+function findLargestPrimeFactor(num) {
+  const isPrime = (num) => {
+    // Trial Divison Algortihm
+
+    const rootOfNum = Math.ceil(Math.sqrt(num));
+
+    for (let i = 2; i <= rootOfNum; i++) {
+      if (num % i == 0) return false;
+    }
+    return true;
+  };
+  let largest = -Infinity;
+
+  let factor = 2;
+  while (factor <= num) {
+    if (isPrime(factor)) {
+      if (num % factor == 0) {
+        largest = factor;
+        num /= factor;
+      }
+    }
+    factor++;
+  }
+  return largest;
+}
