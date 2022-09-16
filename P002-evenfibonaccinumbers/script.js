9a138133a712d8aa2796f7ea4925fccc218f415e
neglux <akcaaytuna@gmail.com>
2022-09-16 09:49:56 +0300

    improving solution with Binet's Formula

--- P002-evenfibonaccinumbers/script.js
+++ P002-evenfibonaccinumbers/script.js
@@ -1,19 +1,13 @@
-function evenFibonacciNumbers(range) {
-  if (range < 2) return 0;
+/* Binet's Fibonacci Number Formula */
+function evenFibonacciNumbers(limit) {
+  const fib = (n) => Math.round(Math.pow(1.61803398875, n) / Math.sqrt(5));
+  let evenTotal = 0;
 
-  let left = 1;
-  let right = 2;
-  let evenTotal = 2;
-
-  let counter = 0;
-  while (counter < range - 2) {
-    const number = left + right;
-    if (number % 2 === 0) evenTotal += number;
-
-    left = right;
-    right = number;
-    counter++;
+  let index = 1;
+  while (true) {
+    const curFib = fib(index);
+    if (curFib >= limit) return evenTotal;
+    if (curFib % 2 === 0) evenTotal += curFib;
+    index++;
   }
-
-  return evenTotal;
 }
