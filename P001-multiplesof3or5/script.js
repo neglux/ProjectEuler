7d07aadcf946d529c0caa8afb347e063afb88e92
neglux <akcaaytuna@gmail.com>
2022-09-18 08:21:33 +0300

    removing O(n) solution and improving naming

--- P001-multiplesof3or5/script.js
+++ P001-multiplesof3or5/script.js
@@ -1,32 +1,15 @@
-/* O(n) solution */
-function sumOfMultiples(range) {
-  const multiples = [];
-  for (let i = 0; i < range; i++) {
-    if (!multiples.includes(i)) {
-      if (i % 3 === 0) multiples.push(i);
-      else if (i % 5 === 0) multiples.push(i);
-    }
-  }
-  let total = 0;
-  for (let i = 0; i < multiples.length; i++) total += multiples[i];
-
-  return total;
-}
-
-/* O(1) solution */
-function sumOfMultiples(range) {
-  const findMaxMultiple = (range, number) => {
-    for (let i = range - 1; i != 0; i--) if (i % number == 0) return i;
+function sumOfMultiples(a, b, limit) {
+  const getLargestMultiple = (num, limit) => {
+    for (let i = limit - 1; i != 0; i--) if (i % num == 0) return i;
+    return null;
   };
-  const getTotalMultiplesOfNumber = (range, number) => {
-    const maxMultiple = findMaxMultiple(range, number);
+  const addMultiples = (num, limit) => {
+    const maxMultiple = getLargestMultiple(num, limit);
     if (!maxMultiple) return 0;
-    const n = maxMultiple / number;
-    return number * ((n * (n + 1)) / 2);
+    const n = maxMultiple / num;
+    return num * ((n * (n + 1)) / 2);
   };
   return (
-    getTotalMultiplesOfNumber(range, 3) +
-    getTotalMultiplesOfNumber(range, 5) -
-    getTotalMultiplesOfNumber(range, 15)
+    addMultiples(a, limit) + addMultiples(b, limit) - addMultiples(a * b, limit)
   );
 }
