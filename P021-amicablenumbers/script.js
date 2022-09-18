1a9e0ad21c9cc59b3465e66942d48843273a2054
neglux <akcaaytuna@gmail.com>
2022-09-18 18:28:17 +0300

    rafactoring

--- P021-amicablenumbers/script.js
+++ P021-amicablenumbers/script.js
@@ -1,32 +1,20 @@
-function sumAmicableNumbersLessThan(maxValue) {
-  const findAmicableNumbersLessThan = (maxValue) => {
-    const calcDivisorSumLessThanNum = (num) => {
-      const calcDivisors = (num) => {
-        const divisors = [];
-        for (let i = 1; i <= Math.sqrt(num); i++) {
-          if (num % i === 0) {
-            if (num / i !== i) divisors.push(...[i, num / i]);
-            else divisors.push(i);
-          }
-        }
-        return divisors;
-      };
-      return calcDivisors(num).reduce((acc, val) => (acc += val), 0) - num;
-    };
-    const isAmicableNumber = (i) => {
-      let pairOfi = calcDivisorSumLessThanNum(i);
-      if (i === pairOfi) return false;
-      return i === calcDivisorSumLessThanNum(pairOfi);
+import { trialDivision } from "../eulib.js";
+
+function sumOfAmicableNumbers(maxValue) {
+  const isAmicableNumber = (i) => {
+    const sumOfDivisors = (num) => {
+      return trialDivision(num).reduce((acc, val) => (acc += val), 0) - num;
     };
 
-    const amicableNumbers = [];
-    for (let i = 1; i < maxValue; i++) {
-      if (isAmicableNumber(i)) amicableNumbers.push(i);
-    }
-    return amicableNumbers;
+    const pairOfi = sumOfDivisors(i);
+
+    if (i === pairOfi) return false;
+    return i === sumOfDivisors(pairOfi);
   };
-  return findAmicableNumbersLessThan(maxValue).reduce(
-    (acc, val) => (acc += val),
-    0
-  );
+
+  const amicableNumbers = [];
+  for (let i = 1; i < maxValue; i++) {
+    if (isAmicableNumber(i)) amicableNumbers.push(i);
+  }
+  return amicableNumbers.reduce((acc, val) => (acc += val), 0);
 }
