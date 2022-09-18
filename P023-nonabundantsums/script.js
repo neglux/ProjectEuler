0cf04b9f56defba8ab48f54d2481d30f8bd394b4
neglux <akcaaytuna@gmail.com>
2022-09-18 18:29:09 +0300

    trialDivision from eulib.js

--- P023-nonabundantsums/script.js
+++ P023-nonabundantsums/script.js
@@ -1,33 +1,32 @@
-function nonAbundantSums() {
-  const isAbundantNum = (num) => {
-    const getDivisorsLessThanNum = (num) => {
-      if (num === 0) return [];
-      const arr = [1];
-      for (let i = 1; i <= Math.sqrt(num); i++) {
-        if (num % i === 0 && i !== num && num / i !== num) {
-          if (num / i !== i) arr.push(...[i, num / i]);
-          else arr.push(i);
-        }
-      }
-      return arr;
-    };
-    const sum = (arr) => {
-      return arr.reduce((acc, val) => (acc += val), 0);
-    };
-    return sum(getDivisorsLessThanNum(num)) > num;
+import { trialDivision } from "../eulib.js";
+
+function nonAbundantSums(max) {
+  const sum = (arr) => {
+    return arr.reduce((acc, val) => (acc += val), 0);
   };
+  const isAbundantNum = (num) => {
+    return sum(trialDivision(num)) - num > num;
+  };
+  const generateAbudantSequence = (max) => {
+    const obj = {};
+    for (let i = 12; i <= max; i++) {
+      if (isAbundantNum(i)) obj[i] = true;
+    }
+    return obj;
+  };
+
+  const abundants = generateAbudantSequence(max);
   let total = 0;
-  for (let i = 1; i <= 28123; i++) {
+  for (let i = 1; i <= max; i++) {
     let addToTheSum = true;
-    for (let j = i; j >= 12; j--) {
-      if (isAbundantNum(j)) {
-        if (isAbundantNum(i - j)) {
-          addToTheSum = false;
-          break;
-        }
-      }
+    let j = i;
+    while (j >= 12 && addToTheSum) {
+      if (abundants[j] && abundants[i - j]) addToTheSum = false;
+      j--;
     }
     if (addToTheSum) total += i;
   }
   return total;
 }
+
+console.log(nonAbundantSums(28123));
