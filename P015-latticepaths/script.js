c3bd84004eea1993ded6d6b70b83fdcee6c08fda
neglux <akcaaytuna@gmail.com>
2022-09-18 18:27:25 +0300

    binomialCoefficient from eulib.js

--- P015-latticepaths/script.js
+++ P015-latticepaths/script.js
@@ -1,19 +1,14 @@
+import { binomialCoefficient } from "../eulib.js";
+
 /* 
 The number of lattice paths from (0,0) to (n,k) is equal to the binomial coefficient 
    n+k
     n
 */
 function calcNumberOfLatticePaths(startPoint, endPoint) {
-  const factorial = (n) => {
-    if (n === 0 || n === 1) return 1;
-
-    return n * factorial(n - 1);
-  };
-  const binomialCoefficient = (n, k) =>
-    factorial(n) / (factorial(k) * factorial(n - k));
   if (startPoint.i !== 0 && startPoint.j !== 0) return undefined;
 
   return binomialCoefficient(endPoint.i + endPoint.j, endPoint.i);
 }
 
-calcNumberOfLatticePaths({ i: 0, j: 0 }, { i: 20, j: 20 });
+console.log(calcNumberOfLatticePaths({ i: 0, j: 0 }, { i: 20, j: 20 }));
