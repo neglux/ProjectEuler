1acf27692c589825ee9eb846f307d0258684b3b5
neglux <akcaaytuna@gmail.com>
2022-09-18 18:26:44 +0300

    removing console.log

--- P010-summationofprimes/script.js
+++ P010-summationofprimes/script.js
@@ -2,6 +2,5 @@ import { sieveOfEratosthenes } from "../eulib.js";
 
 function sumPrimesInRange(maxValue) {
   const arr = sieveOfEratosthenes(undefined, maxValue);
-  console.log(arr);
   return arr.reduce((acc, value) => (acc += value));
 }
