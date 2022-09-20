ecf4fb58e692068fdcba50a74a026f7e6e57c85a
neglux <akcaaytuna@gmail.com>
2022-09-20 17:41:16 +0300

    adding problem solution

---
+++ P036-doublebasepalindromes/script.js
@@ -0,0 +1,21 @@
+import { isPalindrome } from "../eulib.js";
+
+function sumDoubleBasePalindromes(limit) {
+  const convertToBase2 = (num) => {
+    if (num < 2) return 1;
+
+    return convertToBase2(Math.floor(num / 2)) + "" + (num % 2);
+  };
+
+  const palindromes = [];
+
+  let num = 0;
+  while (num < limit) {
+    const base2 = convertToBase2(num);
+    if (isPalindrome(num) && isPalindrome(base2)) {
+      palindromes.push(num);
+    }
+    num++;
+  }
+  return palindromes.reduce((acc, val) => (acc += val), 0);
+}
