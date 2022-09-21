9456c0bd22a61d3c4b5086b6904a167c833370cc
neglux <akcaaytuna@gmail.com>
2022-09-21 18:54:23 +0300

    isPandigital from eulib.js

--- P032-pandigitalproducts/script.js
+++ P032-pandigitalproducts/script.js
@@ -1,25 +1,6 @@
-import { sum } from "../eulib.js";
+import { sum, isPandigital } from "../eulib.js";
 
 function pandigitalProducts(n) {
-  const isTotalsEqual = (arr1, arr2) => {
-    return sum(arr1) === sum(arr2);
-  };
-
-  const isPandigital = (num, n) => {
-    num = [...String(num)];
-    const from1ToN = Array.from({ length: n }, (_, i) => i + 1);
-
-    if (!isTotalsEqual(num, from1ToN)) return false;
-
-    const seen = {};
-    for (const digit of num) {
-      if (digit == 0) return false;
-      if (digit in seen) return false;
-      seen[digit] = true;
-    }
-    return true;
-  };
-
   const uniques = {};
   for (let mc = 1; mc < 2000; mc++) {
     for (let mr = 1; mr < 2000; mr++) {
@@ -31,3 +12,5 @@ function pandigitalProducts(n) {
   }
   return sum(Object.keys(uniques));
 }
+
+console.log(pandigitalProducts(9));
