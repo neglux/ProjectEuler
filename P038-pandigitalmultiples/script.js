dbfe9de36f62ea5369df87a0273a590b6950e799
neglux <akcaaytuna@gmail.com>
2022-09-24 14:55:24 +0300

    removing unused variable

--- P038-pandigitalmultiples/script.js
+++ P038-pandigitalmultiples/script.js
@@ -2,7 +2,6 @@ import { isPandigital } from "../eulib.js";
 
 function getMaxPandigitalMultiple() {
   for (let num = 9999; num > 0; num--) {
-    let n = 2;
     let str = num + "" + String(num * 2);
     if (isPandigital(str, 9)) return str;
   }
